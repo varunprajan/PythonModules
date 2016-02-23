Made class constants uppercase, fixed calculation of Kic, Kie using potential

cadd_mesh2.py
@@ -7,9 +7,7 @@ import cadd_io as cdio
 
 TOL = 1e-10
 
-class Line(object):
-    coldict = {'x': 0, 'y': 1}
-    
+class Line(object):    
     def __init__(self,startpoint,endpoint):
         self.startpoint = startpoint
         self.endpoint = endpoint
@@ -388,8 +386,9 @@ class Nodes(object):
     def write_types(self,nodespref,subdir):
         filename = '{}.types'.format(subdir+nodespref)
         cdio.write_array(self.types,filename)    
-        
-    def get_nnodes(self):
+    
+    @property
+    def nnodes(self):
         return self.xy.shape[0]
         
     def get_points(self,nodelist):
@@ -401,7 +400,7 @@ class Nodes(object):
     def add_node(self,coords,nodetype):
         self.xy = np.vstack((self.xy,coords))
         self.types = np.vstack((self.types,nodetype))
-        return self.get_nnodes()-1 # count starts at zero
+        return self.nnodes-1 # count starts at zero
     
     def set_node_bc(self,nodelist,bcnum):
         self.types[nodelist,2] = bcnum
@@ -431,7 +430,7 @@ class Nodes(object):
             return self.xy[nodenum,:]
     
     def closest_node(self,pt):
-        if self.get_nnodes() > 0:
+        if self.nnodes > 0:
             dist = np.linalg.norm(self.xy - pt, axis=1)
             return np.argmin(dist)
 

crystallography_oo.py
@@ -32,7 +32,7 @@ class Material(object):
     
     @property
     def voigt_plane_strain_compliance(self):
-        return spla.inv(voigt_plane_strain_stiffness)
+        return spla.inv(self.voigt_plane_strain_stiffness)
             
     def voigt_rotated_stiffness(self,aold,anew):
         stiffnesstensor = voigt_to_tensor(self.voigt_stiffness)
@@ -40,34 +40,34 @@ class Material(object):
         return tensor_to_voigt(stiffnesstensorrot)  
 
     def voigt_rotated_compliance(self,aold,anew):
-        rotatedstiffness = self.rotated_stiffness(aold,anew)
+        rotatedstiffness = self.voigt_rotated_stiffness(aold,anew)
         return spla.inv(rotatedstiffness)
         
     def voigt_rotated_plane_strain_compliance(self,aold,anew,symmoption=False):
-        rotatedstiffness = self.rotated_stiffness(aold,anew)
+        rotatedstiffness = self.voigt_rotated_stiffness(aold,anew)
         planestrainstiffness = plane_strain_stiffness(rotatedstiffness)
         if symmoption: # eliminate shear-normal coupling terms
             planestrainstiffness = elim_coupling(planestrainstiffness)
         return spla.inv(planestrainstiffness)
         
     def aniso_k_const(self,aold,anew,symmoption=False):
-        compliance = self.plane_strain_compliance(aold,anew,symmoption)
+        compliance = self.voigt_rotated_plane_strain_compliance(aold,anew,symmoption)
         return lekh_eigenvalues(compliance)
         
     def surface_energy(self,plane):
-        pass
+        return self.surfaceenergies[plane]
       
     def usf_energy(self,plane,direction):
         pass
 
     def A_const(self,aold,anew):
-        compliance = self.plane_strain_compliance(aold,anew)
-        return get_aniso_fac(compliance)
+        compliance = self.voigt_rotated_plane_strain_compliance(aold,anew)
+        return aniso_fac(compliance)
     
     def kic(self,aold,anew):
         crackplane = anew[1,:]
         crackplanekey = convert_to_string(crackplane,1)
-        gammasurf = self.get_surface_energy(crackplanekey)
+        gammasurf = self.surface_energy(crackplanekey)
         A = self.A_const(aold,anew)
         return np.sqrt(2*gammasurf/A)
         
@@ -88,7 +88,7 @@ class Material(object):
         (nu, mu) = isotropic_constants(compliance)
         slipplanekey = convert_to_string(slipplane,1)
         burgerskey = convert_to_string(burgersvector,1)
-        gammaus = self.get_usf_energy(slipplanekey,burgerskey)
+        gammaus = self.usf_energy(slipplanekey,burgerskey)
         try:
             return np.sqrt(2*mu/(1-nu)*(1 + (1-nu)*np.tan(phi)**2)*gammaus)/fI(theta)
         except ZeroDivisionError:
@@ -102,6 +102,22 @@ class Material(object):
             print('K_ie: {0}'.format(kie))
         return kic, kie
 
+class Hex(Material):
+    def __init__(self,elasticprops,surfaceenergies,usfenergy):
+        self.surfaceenergies = surfaceenergies
+        self.usfenergy = usfenergy
+        super().__init__('hex',elasticprops)
+    
+    @classmethod
+    def from_potential(cls,potential):
+        return cls(elasticprops=potential.elasticdict('hex'),
+                   surfaceenergies=potential.surfacedict('hex'),
+                   usfenergy=potential.usf)
+    
+    def usf_energy(self,plane,direction):
+        if direction == '110' and plane == '211': # full
+            return self.usfenergy
+        
 class FCC(Material):
     def __init__(self,elasticprops,surfaceenergies,usfenergy):
         self.surfaceenergies = surfaceenergies
@@ -203,7 +219,7 @@ def tensor_to_voigt_index(vals):
         sol = Mmath.set_diff(range(3),set(vals))
         return sol[0] + 3
     
-def get_aniso_fac(mat):
+def aniso_fac(mat):
     a11, a12, a22, a66 = mat[0,0], mat[0,1], mat[1,1], mat[2,2]
     return np.sqrt(a11*a22/2)*np.sqrt((2*a12+a66)/(2*a11) + np.sqrt(a22/a11))
 
@@ -232,7 +248,7 @@ def gcd_many(mylist):
         return mylist[0]
 
 # Sih/Leibowitz anisotropic crack fields
-def get_lekh_eigenvalues(mat):
+def lekh_eigenvalues(mat):
     a11, a12, a16, a22, a26, a66 = mat[0,0], mat[0,1], mat[0,2], mat[1,1], mat[1,2], mat[2,2]
     poly = [a11,-2*a16,2*a12+a66,-2*a26,a22]
     eigs = np.roots(poly)

newpotential.py
@@ -19,40 +19,48 @@ def gen_std_potential(ductilityindex,dimensions,r0=r0def):
     return NewPotential(dimensions,r0,ep=epvec[ductilityindex],usf=usfvec[ductilityindex])
     
 def gen_usf_potential(usftarget,dimensions,r0=r0def):
-    ep = estimate_ep(dimensions,r0,usftarget)
-    return NewPotential(dimensions,r0,ep=ep)
+    return NewPotential(dimensions,r0,usftarget=usftarget)
     
 def gen_non_std_potential(dimensions,r0=r0def,**kwargs):
     return NewPotential(dimensions,r0,**kwargs)
     
 class NewPotential(SplinePotential):
-    r2bardef = {2: np.sqrt(2), 3: np.sqrt(3/2)}
-    rnnndef = {2: np.sqrt(3) - 0.02, 3: np.sqrt(2) - 0.02}
-    alphabardef = {2: 3.5*2**(1/6), 3: 6.5*2**(1/6)}
-
-    def __init__(self,dimensions,r0,ep,usf=None,r1bar=1.05,r2bar=None,r3bar=None,rnnn=None,alphabar=None,offset=0.0,spline=None):
+    R2BARDEF = {2: np.sqrt(2), 3: np.sqrt(3/2)}
+    RNNNDEF = {2: np.sqrt(3) - 0.02, 3: np.sqrt(2) - 0.02}
+    ALPHABARDEF = {2: 3.5*2**(1/6), 3: 6.5*2**(1/6)}
+    
+    def __init__(self,dimensions,r0,ep=None,usf=None,r1bar=1.05,r2bar=None,r3bar=None,rnnn=None,alphabar=None,offset=0.0,spline=None,usftarget=None):
         self.dimensions = dimensions
         self.r0 = r0
+        if ep is None:
+            ep = self.estimate_ep(usftarget)
         self.ep = ep
-        self.usf = usf
+        self._usf = usf
         self.r1bar = r1bar
         if r2bar is None:
-            self.r2bar = NewPotential.r2bardef[dimensions]
+            self.r2bar = self.R2BARDEF[dimensions]
         if rnnn is None:
-            self.rnnn = NewPotential.rnnndef[dimensions]
+            self.rnnn = self.RNNNDEF[dimensions]
         if alphabar is None:
-            self.alphabar = NewPotential.alphabardef[dimensions]
+            self.alphabar = self.ALPHABARDEF[dimensions]
         self.r3bar = r3bar
         self.offset = offset
         super().__init__(spline)
-        
+    
+    @property
+    def usf(self):
+        if self._usf is None:
+            return self.estimate_usf()
+        else:
+            return self._usf
+    
     def gen_splines(self,r3barfac=0.98,step=0.01,tol=1.e-8):
         while r3barfac > 0:
             self.r3bar = self.r2bar + (self.rnnn - self.r2bar)*r3barfac
-            def getResidual(coeff):
+            def residual(coeff):
                 self._set_splines_coeff_(coeff)
                 return self._residual_()
-            coeffsol = spo.fsolve(getResidual,np.zeros(8))
+            coeffsol = spo.fsolve(residual,np.zeros(8))
             self._set_splines_coeff_(coeffsol)
             # 2nd spline can be non-attractive; if so, reduce r3bar
             maxval2 = Mmath.max_poly_root(self.spline[1])
@@ -102,44 +110,6 @@ class NewPotential(SplinePotential):
             return self._spline_force_(rbar,1)/self.r0
         else:
             return 0
-    
-    # used for computing Kic, Kie, etc.    
-    def get_K_props(self,lattice):
-        alpha = self.alphabar/self.r0
-        # for closed-packed slip
-        if self.usf is None:
-            self.usf = self.estimate_usf()
-        # lattice-dependent properties
-        if lattice == 'hex':
-            symmetry = 'cubic'
-            C11 = 3/2*np.sqrt(3)*alpha**2
-            Velastic = {'11': C11, '12': C11/3, '44': C11/3}
-            Vsurface = {'112': {'surf': 1/self.r0}}
-        elif lattice == 'fcc':
-            symmetry = 'cubic'
-            C11 = 2*np.sqrt(2)*alpha**2/self.r0
-            Velastic = {'11': C11, '12': C11/2, '44': C11/2}
-            Vsurface = {'111': {'surf': np.sqrt(3)/self.r0**2}, '001': {'surf': 2/self.r0**2}, '011': {'surf': np.sqrt(9/2)/self.r0**2}}
-        elif lattice == 'hcp':
-            symmetry = 'hexagonal'
-            C11 = 5/np.sqrt(2)*alpha**2/self.r0
-            Velastic = {'11': C11, '33': C11*16/15, '13': C11*4/15, '44': C11*4/15, '66': C11/3}
-            Vsurface = {'001': {'surf': 1.458*2**(1/3)/self.r0**2}}
-        return {'elastic': Velastic, 'surface': Vsurface, 'unstable': {'full': self.usf, 'partial': self.usf}, 'symmetry': symmetry, 'rho': rho}
-        
-    def estimate_ep(self,usftarget):
-        """
-        Estimates phi(r2) = ep required to achieve
-        a desired gamma_us
-        """
-        return -((usftarget - self.offsetusf)*self.area - 1)/2
-
-    def estimate_usf(self,ep):
-        """
-        Estimates gamma_us for the potential, using phi(r2) = ep
-        and the offset value (obtained from molecular statics)
-        """
-        return (1 - 2*np.abs(ep))/self.area + self.offset_usf
 
     def elasticdict(self,lattice):
         alpha = self.alphabar/self.r0
@@ -154,16 +124,21 @@ class NewPotential(SplinePotential):
             elif lattice == 'hcp':
                 C11 = 5/np.sqrt(2)*alpha**2/self.r0
                 return {'11': C11, '33': C11*16/15, '13': C11*4/15, '44': C11*4/15, '66': C11/3}
-        raise ValueError('Undefined lattice')
     
-    @property
     def surfacedict(self,lattice):
         if lattice == 'hex':
-            return {'112': 1/self.r0}
+            return {'211': 1/self.r0, '100': 1/self.r0}
         elif lattice == 'fcc':
-            return {'111': np.sqrt(3)/self.r0**2, '001': 2/self.r0**2, '011': np.sqrt(9/2)/self.r0**2}
+            return {'111': np.sqrt(3)/self.r0**2, '100': 2/self.r0**2, '110': np.sqrt(9/2)/self.r0**2}
         elif lattice == 'hcp':
-            return {'001': 1.458*2**(1/3)/self.r0**2}       
+            return {'100': 1.458*2**(1/3)/self.r0**2}
+
+    def unstabledict(self,lattice):
+        if lattice == 'hex':
+            dislocationtype = 'full'
+        elif lattice in ['fcc','hcp']:
+            dislocationtype = 'partial'
+        return {dislocationtype: self.usf}
         
     @property
     def rho(self):
@@ -182,7 +157,21 @@ class NewPotential(SplinePotential):
             return self.r0
         elif self.dimensions == 3:
             return np.sqrt(3)/2*self.r0**2
-    
+       
+    def estimate_ep(self,usftarget):
+        """
+        Estimates phi(r2) = ep required to achieve
+        a desired gamma_us
+        """
+        return -((usftarget - self.offsetusf)*self.area - 1)/2
+
+    def estimate_usf(self):
+        """
+        Estimates gamma_us for the potential, using phi(r2) = ep
+        and the offset value (obtained from molecular statics)
+        """
+        return (1 - 2*np.abs(self.ep))/self.area + self.offset_usf
+            
     @property
     def offset_usf(self):
         """